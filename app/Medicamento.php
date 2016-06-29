c6ca0aa99ef6f5bd77e9fe417608c2e61f0d920b
marcoslunah <mlunag16@gmail.com>
2016-06-28 23:01:27 -0400

    Modelo de Medicamentos

---
+++ app/Medicamento.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Medicamento extends Model
+{
+
+    protected $table = 'medicamento';
+
+    protected $fillable = [
+        'nombre',
+        'laboratorio',
+        'concentracion',
+        'forma_farmacetica',
+        'titular',
+        'nombre_generico',
+    ];
+
+
+    public function crearNuevo($request)
+    {
+        return $this->create($request);
+    }
+
+    public function buscarPorId($id)
+    {
+        return $this->where('id',$id)->first();
+    }
+
+    public function buscarPorNombre($nombre)
+    {
+        return $this->where('nombre', 'LIKE', '%' . $nombre . '%')->take(10)->get();
+    }
+}
