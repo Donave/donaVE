806964107e767e4efddcaf149dbd95b6a64de055
marcoslunah <mlunag16@gmail.com>
2016-07-05 11:44:07 -0400

    Migracion de Acceso a la Api

---
+++ database/migrations/2016_07_01_014451_create_api_acceso_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApiAccesoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_acceso', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre_cliente');
+            $table->string('token_acceso');
+            $table->boolean('estado');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
