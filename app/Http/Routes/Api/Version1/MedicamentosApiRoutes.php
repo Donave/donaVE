fd44eaa14ac31f8cb73d8badaf003095e53b223a
marcoslunah <mlunag16@gmail.com>
2016-07-04 22:39:37 -0400

    incluyendo el Middleware de Acceso a las rutas de Medicamentos

--- app/Http/Routes/Api/Version1/MedicamentosApiRoutes.php
+++ app/Http/Routes/Api/Version1/MedicamentosApiRoutes.php
@@ -6,9 +6,11 @@
  * Time: 8:40 PM
  */
 
-$api->version('v1', function ($api) {
+$api->version('v1',['middleware' => '\App\Http\Middleware\AccesoApi::class'], function ($api) {
 
-    $api->get('medicamentos', 'App\Http\Controllers\MedicamentosController@getTodos');
-    $api->get('medicamento/{id}', 'App\Http\Controllers\MedicamentosController@show');
-    $api->get('medicamentos/nombre/{nombre}', 'App\Http\Controllers\MedicamentosController@getBuscarPorNombre');
+        $api->get('medicamentos', 'App\Http\Controllers\MedicamentosController@getTodos');
+        $api->get('medicamento/{id}', 'App\Http\Controllers\MedicamentosController@show');
+        $api->get('medicamentos/nombre/{nombre}', 'App\Http\Controllers\MedicamentosController@getBuscarPorNombre');
+
+
 });
