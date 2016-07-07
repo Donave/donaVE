17ecc937dc8e4cd84224d86b50d5e962e1fc68ea
marcoslunah <mlunag16@gmail.com>
2016-07-07 09:22:56 -0400

    Migracion de Solicitud

---
+++ database/migrations/2016_07_05_155243_create_solicitud_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSolicitudTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('solicitud', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('correo_electronico');
+            $table->string('id_url');
+            $table->enum('tipo',['medicamento'])->default('medicamento');
+            $table->integer('id_elemento');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('solicitud');
+    }
+}
