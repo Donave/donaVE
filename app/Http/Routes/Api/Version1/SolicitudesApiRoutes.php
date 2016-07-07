0d2f796132e47c882685ac944daf6ae5d93330ef
marcoslunah <mlunag16@gmail.com>
2016-07-07 09:24:51 -0400

    Rutas de Solicitud

---
+++ app/Http/Routes/Api/Version1/SolicitudesApiRoutes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcos
+ * Date: 7/05/2016
+ * Time: 8:40 PM
+ */
+
+$api->version('v1',['middleware' => '\App\Http\Middleware\AccesoApi::class'], function ($api) {
+
+    $api->get('solicitudes/activas/{pagina}', 'App\Http\Controllers\SolicitudController@getSolitudesActivas');
+    $api->get('solicitudes/finalizadas/{pagina}', 'App\Http\Controllers\SolicitudController@getSolitudesFinalizadas');
+
+
+});
